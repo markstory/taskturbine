764a03ad0ca959a5a6fbc23bd592f3027c9a6297
Mark Story <mark@mark-story.com>
2025-12-20 11:02:13 -0500

    Fix spacing

--- demo-app/src/db.rs
+++ demo-app/src/db.rs
@@ -8,6 +8,7 @@ pub async fn create_db() -> Pool<Postgres> {
     let database_log_queries = env::var("DEMO_DATABASE_LOG_QUERIES").unwrap_or("false".into());
     let pool = PgPool::connect_lazy(&database_url)
         .expect("Failed to create database connection pool");
+
     let options: Result<PgConnectOptions, _> = database_url.parse();
     if let Ok(mut opts) = options {
         if database_log_queries == "true" {
