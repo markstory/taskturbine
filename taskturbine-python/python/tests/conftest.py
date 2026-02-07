f973894d4fe792767307f37dee8963815e57b87f
Mark Story <mark@mark-story.com>
2026-02-06 16:21:07 -0800

    Fix bleedthrough between tests

--- taskturbine-python/python/tests/conftest.py
+++ taskturbine-python/python/tests/conftest.py
@@ -4,6 +4,16 @@ import pytest
 
 from taskturbine import Config
 
+def pytest_sessionstart():
+    # Clear storage tables at the beginning of each run
+    db_url = os.getenv("TASKTURBINE_DATABASE_URL")
+    assert db_url, "Required environment variable TASKTURBINE_DATABASE_URL undefined"
+
+    connection = psycopg2.connect(db_url)
+    cursor = connection.cursor()
+    for table in ("events", "waits", "runs", "tasks"):
+        cursor.execute(f"DELETE FROM taskturbine.{table}")
+
 
 @pytest.fixture
 def channel(request):
