0335897bc6b1da820832cd9f446b2e9745b7c103
Mark Story <mark@mark-story.com>
2026-02-28 23:52:51 -0500

    Expand the example app with a prototype of an API design.
    This is quite a bit different than the rust interface, but I also think
    it makes more sense for python. I also don't know how to do this in rust
    yet.

--- taskturbine-python/python/testapp.py
+++ taskturbine-python/python/testapp.py
@@ -1,5 +1,8 @@
+from datetime import time
 import logging
 import os
+from typing import Any
+import uuid
 
 from taskturbine import TaskturbineApp, Config
 from taskturbine.context import TaskContext
@@ -15,9 +18,47 @@ config = Config(
 )
 app = TaskturbineApp(config)
 
+type Payload = dict[str, Any]
+
 @app.register_task("hello-world")
 def hello_world(ctx: TaskContext) -> None:
     print(f"Hello world! {ctx.params_bytes.decode()}")
+
+    # We start off defining the steps in our task.
+    @ctx.step(name="compute_user_data")
+    def compute_user_data(ctx: TaskContext) -> Payload:
+        logging.info("starting compute_user_data")
+        # a step that does some compute/query and returns a dict value.
+        params = ctx.params()
+        return {"name": params["name"], "id": uuid.uuid4().hex, "started": time.time()}
+
+
+    # a step that fails when the context counter is at the wrong value
+    #   on the second retry enough time has elapsed that it continues and returns a value.
+    @ctx.step(name="delay_via_retry_errors")
+    def check_duration(ctx: TaskContext, user: Payload) -> None:
+        logging.info("starting check_duration")
+        now = time.time()
+        if now - user["started"] < 3:
+            raise ValueError("Too soon, die and retry")
+
+    @ctx.step(name="process-complete")
+    def process_complete(ctx: TaskContext, user: Payload, event: Payload) -> Payload:
+        logging.info("starting process_complete")
+        # Do some IO
+        user["complete"] = True
+        user["event_id"] = event["id"]
+        return user
+        
+        
+    # Once we have defined our steps, we wire them together with control flow.
+    user = compute_user_data(ctx)
+    check_duration(ctx, user)
+    # wait until we hear from an external workflow
+    event = ctx.await_event(f"verified-{user['name']}")
+    # a step that uses the event results and other results to make a new value.
+    completed = process_complete(ctx, user, event)
+    logging.info("Completed:", completed)
 
 
 def main() -> None:
