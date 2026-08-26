5cc82521a7abee365195025becfc5b59445d7c15
Mark Story <mark@mark-story.com>
2026-08-26 00:00:01 -0400

    Simpler schema
    Since runs already have an available_at time, having a wait timeout
    seems redundant. Instead, we can rely on await_event() setting the sleep
    time, and upkeep loops/retries continuing the sleep cycle until the task
    max ages out, or is fulfilled.

--- taskturbine-core/migrations/0003_run_wait_event.sql
+++ taskturbine-core/migrations/0003_run_wait_event.sql
@@ -1,3 +1,2 @@
 ALTER TABLE taskturbine.runs ADD COLUMN wait_event_name TEXT;
-ALTER TABLE taskturbine.runs ADD COLUMN wait_timeout TIMESTAMPTZ;
 CREATE INDEX runs_wait_event ON taskturbine.runs (wait_event_name);
