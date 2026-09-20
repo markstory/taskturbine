1889ed3372acb0a48fac48d1676b2d76a04466f4
Mark Story <mark@mark-story.com>
2026-09-20 13:46:46 -0400

    Update CLI tools for changes in config
    App config will propagate more nicely now.

--- taskturbine-cli/src/task_spawn.rs
+++ taskturbine-cli/src/task_spawn.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 use clap::Args;
+use taskturbine::config::Config;
 
 use crate::CliError;
 use taskturbine_core::storage::{Storage, TaskOptions};
@@ -97,13 +98,13 @@ impl From<SpawnArgs> for TaskOptions {
 }
 
 /// Spawn a task based on the command like parameters.
-pub async fn spawn_task(storage: Storage, args: SpawnArgs) -> Result<(), CliError> {
+pub async fn spawn_task(storage: Storage, config: Config, args: SpawnArgs) -> Result<(), CliError> {
     let taskname = args.taskname.clone();
 
     let channel_name = args
         .channel
         .clone()
-        .unwrap_or(storage.get_config().default_channel);
+        .unwrap_or(config.default_channel);
     log::info!("Spawning task in channel={channel_name} for task={taskname}");
 
     let params = args.params.clone().unwrap_or("{\"args\":[]}".to_string());
--- taskturbine-cli/src/upkeep.rs
+++ taskturbine-cli/src/upkeep.rs
@@ -1,12 +1,13 @@
 use crate::CliError;
 use taskturbine::app::{TaskturbineApp, run_upkeep_worker};
+use taskturbine::config::Config;
 use taskturbine_core::storage::Storage;
 
 /// Perform periodic upkeep operations on all channels in a usecase.
-pub async fn upkeep(storage: Storage) -> Result<(), CliError> {
+pub async fn upkeep(storage: Storage, config: Config) -> Result<(), CliError> {
     log::info!("Starting upkeep worker");
 
-    let app = TaskturbineApp::from_storage(storage);
+    let app = TaskturbineApp::with_storage(config, storage);
     let worker = app.create_worker("cleanup-worker-1", vec![]);
     run_upkeep_worker(worker).await;
     log::info!("Shutdown upkeep worker");
